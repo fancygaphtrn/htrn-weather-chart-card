cd04001fcd218c2dc76b0248ca97964a1f2fbb9f
fancygaphtrn <fancygaphtrn@gmail.com>
2025-07-30 09:24:21 -0400

    Make forecast wind details clickable.

--- dist/htrn-weather-chart-card.js
+++ dist/htrn-weather-chart-card.js
@@ -18955,6 +18955,7 @@ updateChart({ forecasts, forecastChart } = this) {
           align-items: center;
           font-weight: 300;
           padding: 0px 10px 0px 10px;
+          cursor: pointer;
         }
         .wind-detail {
           display: flex;
@@ -19354,7 +19355,7 @@ renderWind({ config, weather, windSpeed, windDirection, forecastItems } = this) 
   const forecast = this.forecasts ? this.forecasts.slice(0, forecastItems) : [];
 
   return x`
-    <div class="wind-details">
+    <div class="wind-details" @click="${(e) => this.showMoreInfo(config.entity)}">
       ${showWindForecast ? x`
         ${forecast.map((item) => {
           let dWindSpeed = item.wind_speed;
